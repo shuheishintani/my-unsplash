709ee3cf78ce663d54fd16dd7552db8231df5963
shuheishintani <shuheishintani.dev@gmail.com>
2020-11-28 13:54:34 +0900

    キーワード検索を実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,21 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 import { FormDialog } from '@/components/FormDialog';
+import { SearchBar } from '@/components/SearchBar';
 import { Photo } from '@/types';
 import { Button, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Masonry from 'react-masonry-component';
 
-async function fetchPhotos() {
-  const response = await fetch('/api/photos');
+const fetchPhotos: (key: string, keyword: string) => Promise<Photo[]> = async (
+  _key,
+  keyword
+) => {
+  const response = await fetch(`/api/photos?keyword=${keyword}`);
   const data = await response.json();
   const { photos } = data;
   return photos;
-}
+};
 
 const isError = (error: unknown): error is Error => {
   return error instanceof Error;
@@ -24,7 +28,11 @@ const useStyles = makeStyles({
 export default function Home() {
   const classes = useStyles();
   const [open, setOpen] = useState<boolean>(false);
-  const { status, data: photos, error } = useQuery('photos', fetchPhotos);
+  const [keyword, setKeyword] = useState<string>('');
+  const { status, data: photos, error } = useQuery(
+    ['photos', keyword],
+    fetchPhotos
+  );
 
   if (status === 'loading') {
     return <p>Loading...</p>;
@@ -37,13 +45,14 @@ export default function Home() {
   }
   return (
     <>
+      <SearchBar setKeyword={setKeyword} />
       <Button onClick={() => setOpen(true)} variant="contained" color="primary">
         Add a Photo
       </Button>
       <FormDialog open={open} setOpen={setOpen} />
       {photos && (
         <Masonry>
-          {photos.map((photo: Photo) => (
+          {photos.map(photo => (
             <Box key={photo.id} className="item" m={2}>
               <img src={photo.url} width="400px" className={classes.img} />
             </Box>
